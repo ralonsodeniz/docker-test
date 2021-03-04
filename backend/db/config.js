91373126140ccbd092e8273c9f0fa38db4024766
ralonsodeniz <ralonsodeniz@gmail.com>
2021-03-04 18:15:54 +0100

    CI/CD github actions + GKE

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-exports.initMongoose = () => mongoose.connect('mongodb://mongodb:27017', (err) => {
+exports.initMongoose = () => mongoose.connect('mongodb://10.46.131.209:27017', (err) => {
     if (err) throw err;
     console.log('Connected to mongodb');
 });
